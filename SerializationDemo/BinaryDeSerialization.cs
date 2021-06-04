19d0c0efb97650fa1c43f3f089f0c648a3e2503f
shivam satpute <shivam897satpute@gmail.com>
2021-06-04 12:58:35 +0530

    [shivam]add: Serialization and Deserialization Demo

---
+++ SerializationDemo/BinaryDeSerialization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace SerializationDemo
+{
+    class BinaryDeSerialization
+    {
+        public void DeSerialization()
+        {
+            FileStream fileStream = new FileStream(@"/Example.txt", FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Demo deserializationdemo = (Demo)formatter.Deserialize(fileStream);
+            int a = 5;
+            //Console.WriteLine("Value of a is "+a);
+            //Console.WriteLine("Value of a is {0}", a);
+            //Console.WriteLine($"Value of a is {a}");
+            Console.WriteLine($"Application name {deserializationdemo.ApplicationName} ---ApplicationId    {deserializationdemo.ApplicationId}");
+            Console.ReadKey();
+        }
+    }
+}
